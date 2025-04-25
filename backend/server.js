ec740c75aa83fbde8048e23f61927994bed64448
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2025-04-25 16:55:29 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/error");
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const videoRoutes = require("./routes/videos");
+const commentRoutes = require("./routes/comments");
+
+// Connect to DB
+connectDB();
+
+// Express App
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
+app.use(cookieParser());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+
+app.use(errorHandler);
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+
+app.listen(port, () => {
+  console.log("Server started listening on ", port);
+});
